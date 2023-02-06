482cfa1972b06b1f3156af704e53c36d899befe3
pratiksj <pratiksharai728@gmail.com>
2023-02-06 22:50:07 +0545

    feat: created route that allow admin to change the status of user's account

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 const { Op } = require("sequelize");
-const { SECRET } = require("../utils/config");
+//const { SECRET } = require("../utils/config");
+const { tokenExtractor } = require("../utils/middleware");
 
 const { Note, User } = require("../models");
 
@@ -10,19 +11,19 @@ const noteFinder = async (req, res, next) => {
   next();
 };
 
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
+// const tokenExtractor = (req, res, next) => {
+//   const authorization = req.get("authorization");
+//   if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+//     try {
+//       req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+//     } catch {
+//       return res.status(401).json({ error: "token invalid" });
+//     }
+//   } else {
+//     return res.status(401).json({ error: "token missing" });
+//   }
+//   next();
+// };
 
 router.get("/", async (req, res) => {
   const where = {};
