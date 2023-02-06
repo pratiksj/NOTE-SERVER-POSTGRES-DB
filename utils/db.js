39be2137053afab8bfd8a4657330f645257a0baa
pratiksj <pratiksharai728@gmail.com>
2023-02-06 22:56:37 +0545

    feat:wrote down npm script which allows to undo the previous migration

--- utils/db.js
+++ utils/db.js
@@ -11,20 +11,28 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
+const migrationConf = {
+  migrations: {
+    glob: "migrations/*.js",
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
 const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: "migrations/*.js",
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
+  const migrator = new Umzug(migrationConf);
 
   const migrations = await migrator.up();
   console.log("Migrations up to date", {
     files: migrations.map((mig) => mig.name),
   });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
 };
 
 const connectToDatabase = async () => {
@@ -43,4 +51,5 @@ const connectToDatabase = async () => {
 module.exports = {
   sequelize,
   connectToDatabase,
+  rollbackMigration,
 };
